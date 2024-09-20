ca241eb3f05c36d58e89e0bd83c69a10fbc76810
Lijuan Wang <2738042w@student.gla.ac.uk>
2024-09-20 13:05:10 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('hello world!');
+let count = 20;
+if (count < 50)
+    count += 30;
+console.log(count);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+let count = 208_899_888 // 可以推测出是number
+let course: string = "typescript"
+let is_pulished: boolean = true
+let level //不知道啥类型
+
+
+let numbers: number[] = [1, 2, 3]
+numbers.forEach(n => n.toString)
