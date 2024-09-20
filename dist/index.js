e58521f335a9ff79604c88bc9a54496cd380955d
Lijuan Wang <2738042w@student.gla.ac.uk>
2024-09-20 14:25:20 +0100

    fundamentals

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,17 @@
 "use strict";
-console.log('hello world!');
-let count = 20;
-if (count < 50)
-    count += 30;
-console.log(count);
+let count = 208899888;
+let course = "typescript";
+let is_pulished = true;
+let level;
+let numbers = [1, 2, 3];
+numbers.forEach(n => n.toString);
+let user = [1, 'lijuan'];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,36 @@ let course: string = "typescript"
 let is_pulished: boolean = true
 let level //不知道啥类型
 
-
+// arrays
 let numbers: number[] = [1, 2, 3]
 numbers.forEach(n => n.toString)
+
+// tuple
+let user: [number, string] = [1, 'lijuan']
+
+// enum
+enum Size {Small = 1, Medium, Large}
+let mySize: Size = Size.Medium
+console.log(mySize)
+
+// function
+function calculate(income: number, taxYear = 2020): number {
+    if(taxYear < 2020)
+        return income * 1.2
+    return income * 1.3
+}
+
+calculate(10_000, 2023)
+
+// objects
+let employee: {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = {
+    id: 1,
+    name: 'lijuan',
+    retire: (date: Date) => {
+        console.log(date)
+    }
+}
